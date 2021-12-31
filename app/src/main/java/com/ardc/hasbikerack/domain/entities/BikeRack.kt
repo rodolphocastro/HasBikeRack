fdb5b96b4e4d0591a8024bec828caf61b8213e68
Rodolpho Alves <rodolpho.castro.a@gmail.com>
2021-12-31 15:28:36 -0300

    feat: Add coordinates to a Bike Rack

--- app/src/main/java/com/ardc/hasbikerack/domain/entities/BikeRack.kt
+++ app/src/main/java/com/ardc/hasbikerack/domain/entities/BikeRack.kt
@@ -1,4 +1,17 @@
 package com.ardc.hasbikerack.domain.entities
 
-data class BikeRack(val name: String, val quantityOfSpots: Int) {
+/**
+ * A set of Coordinates in a 2d axis.
+ */
+data class Coordinates(val x: Double, val y: Double) {
+
+}
+
+/**
+ * A Bicycle Rack where one can park his/hers bicycle.
+ * @param name The name of this rack
+ * @param quantityOfSpots The number of spots this rack has (aka: how many bikes can park here)
+ * @param coords The latitude/longitude for this rack
+ */
+data class BikeRack(val name: String, val quantityOfSpots: Int, val coords: Coordinates) {
 }
--- app/src/test/java/com/ardc/hasbikerack/domain/entities/BikeRackTest.kt
+++ app/src/test/java/com/ardc/hasbikerack/domain/entities/BikeRackTest.kt
@@ -8,16 +8,18 @@ import org.junit.Test
 class `Bike Rack tests` {
     private val bogusName = "The bad place"
     private val bogusQtyOfSpots = 42
+    private val bogusCoords = Coordinates(1.0, 1.0)
 
     @Test
-    fun `Given a name and a number of spots, when constructed, then a Rack is created`() {
+    fun `Given a name, a number of spots and a set of coordinates, when constructed, then a Rack is created`() {
         // Arrange
 
         // Act
-        val got = BikeRack(bogusName, bogusQtyOfSpots)
+        val got = BikeRack(bogusName, bogusQtyOfSpots, bogusCoords)
 
         // Assert
         assertEquals(bogusName, got.name)
         assertEquals(bogusQtyOfSpots, got.quantityOfSpots)
+        assertEquals(bogusCoords, got.coords)
     }
 }
